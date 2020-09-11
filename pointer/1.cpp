0734a5d255df540754110e14abdf1ce628c3723c
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-11 10:00:46 +0700

    logic finish

--- pointer/1.cpp
+++ pointer/1.cpp
@@ -1,37 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-char checkPalin(char , char, char, char);
 int main() {
 	int check = 1;
-	char str[50] ;
-	char *p,*t;
+	char str[50];
+	char* p, * t;
 	scanf("%s", &str);
 	p = str;
 	t = str;
-	while (*p != '\0') {		
-	//printf("%c", *p);
+	while (*p != '\0') {
+		//printf("%c", *p);
+		if (*p >= 'A' && *p <= 'Z') {
+			*p = *p - 'A' + 'a';
+			printf("%c", *p);
+		}
 		p++;
 	}
 	p--;
-	printf("t= %c\np= %c\n", *t, *p);
-	checkPalin(str[0],*p,*t,check);
-	if (check == 0)
-		printf("\nNot Palindrome");
-	else
-		printf("\nPalindrome");
-	return 0;
-}
-char checkPalin(char str,char &p, char *t, char check) {
-	while (*p >= str) {
+	
+	while (p >= str) {	
 		if (*p == *t) {
-			printf("%c", *p);
+			printf("%c = %c\n", *p,*t);
 		}
 		else {
+			printf("%c != %c\n",*t,*p);
 			check = 0;
 			break;
 		}
 		p--;
 		t++;
 	}
-	return check;
+	if (check == 0)
+		printf("\nNot Palindrome");
+	else
+		printf("\nPalindrome");
+	return 0;
 }
