70cc18c8a51b8c3cd2e7436ca19dd8c7c002c9a7
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-11 12:06:41 +0700

    stuck at loop2

--- pointer/2.cpp
+++ pointer/2.cpp
@@ -6,21 +6,41 @@ int main() {
 	scanf("%s", str2);
 	p1 = str1;
 	p2 = str2;
-	
 	while (*p1 != '\0' || *p2 != '\0') {
-		while (*p1 != *p1 + 1) {	p1++; }
-		while (*p2 != *p2 + 1) {	p2++; }
+		printf("\nbefore  p1= %c ", *p1);
+		printf("\nin loop 1..");
+		if (*p1 == (*p1++)) { 
+			printf("\nif  p1= %c ", *p1);
+			printf("\nin loop 2..5555");
+			
+			while (*p1 != *p1++) {
+				printf("\nin loop 2..");
+				printf("\np1= %c ", *p1);
+				p1++;
+			}
+			
+		}
+		printf("\nafter  : p1= %c ", *p1);
+		if (*p2 == *p2 + 1) {
+			while (*p2 != *p2 + 1) {
+				printf("\nin loop 3..");
+				printf("\np2= %c ", *p2);
+				p2++;
+			}
+
+		}
 		if (*p1 == *p2) {
-				printf("%c = %c ", *p1, *p2);
+			printf("\nin if 1..");
+				printf("\n%c = %c ", *p1, *p2);
 		}
 		else {
 				check = 1;
 				break;
 			}
 		p1++;
+		printf("\np1,2++");
 		p2++;
 		}
-	
 	if (check == 0) {
 		printf("\nequal");
 	}
